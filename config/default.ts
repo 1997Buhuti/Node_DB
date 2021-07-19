0ea6446f3b0bfda50a4e5fe03b2604b069555126
Manakal <dp.manakal82@gmail.com>
2021-07-19 18:27:19 +0530

    new version mongo database connected

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
     port:1337,
     host:'localhost',
-    dbUri:'mongodb://localhost:27017/rest-api'
+    dbUri:'mongodb+srv://Manakal:Buhuti2021@cluster0.x1eyy.mongodb.net/Products_DB?retryWrites=true&w=majority'
 };
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import express from 'express';
+const express=require('express');
 import config from 'config';
 import log from "./logger";
+import connect from "./db/connect";
+import mongoose from "mongoose"
 
 const port=config.get("port") as number;
 const host=config.get("host") as string;
 
 const app=express();
+const dbUri='mongodb+srv://Manakal:Buhuti2021@cluster0.x1eyy.mongodb.net/Products_DB?retryWrites=true&w=majority'
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -13,7 +16,15 @@ app.use(express.urlencoded({extended:false}));
 app.listen(port,host,()=>{
     log.info(`Server listening  at https://${host}:${port}`)
 });
-app.get('',(req,res)=>{
-    res.send('hello express I m software engineer bi ');
-})
+mongoose.connect(dbUri,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>{
+        log.info("database connected");
+    })
+    .catch((error)=>{
+        log.error("db error",error);
+        process.exit(1)
+    });
 
