7924166980c527ef91b26efefbe69fce715b5044
Manakal <dp.manakal82@gmail.com>
2021-07-19 21:53:50 +0530

    new version routes created to fetch data from mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,58 @@
-const express=require('express');
+import express from 'express';
 import config from 'config';
 import log from "./logger";
-import connect from "./db/connect";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import Product from  "./models/products"
+import morgan from'morgan';
 
 const port=config.get("port") as number;
 const host=config.get("host") as string;
 
 const app=express();
-const dbUri='mongodb+srv://Manakal:Buhuti2021@cluster0.x1eyy.mongodb.net/Products_DB?retryWrites=true&w=majority'
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+/*
 app.listen(port,host,()=>{
     log.info(`Server listening  at https://${host}:${port}`)
-});
+});*/
+const dbUri='mongodb+srv://Manakal:Buhuti2021@cluster0.x1eyy.mongodb.net/Products_DB?retryWrites=true&w=majority'
+
+//mongoose connect function
 mongoose.connect(dbUri,{
         useNewUrlParser:true,
         useUnifiedTopology:true,
     })
-    .then(()=>{
-        log.info("database connected");
+    .then((result)=>{
+        app.listen(3000)
+        //log.info("database connected");
+        console.log("database connected");
     })
     .catch((error)=>{
-        log.error("db error",error);
-        process.exit(1)
+        //console.log("models error",error);
     });
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+//routes
+app.get('/',(req,res)=>{
+    Product.find().then((result)=>{
+        res.send(result);
+    }).catch((error)=>{
+        console.log(error);
+    });
+})
+app.get('/get-products',(req,res)=>{
+    Product.find().then((result)=>{
+        res.send(result);
+    }).catch((error)=>{
+        console.log(error);
+    });
+})
 
---
+++ src/models/products.ts
@@ -0,0 +1,49 @@
+import { Schema} from 'mongoose';
+import * as mongoose from "mongoose";
+
+interface IProduct {
+    id: number
+    image: string
+    name: string
+    category: string
+    qty: number | 1
+    price: number
+    discount: number
+    total: number
+}
+
+const productSchema= new Schema<IProduct>({
+    id:{
+        type:Number,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    qty:{
+        type:Number,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    discount:{
+        type:Number,
+        required:true
+    },
+    total:{
+        type:Number,
+        required:true
+    },
+
+},{timestamps:true})
+
+const Product=mongoose.model<IProduct>('product',productSchema);
+
+export default Product;
