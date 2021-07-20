83c3fba40b629ce9d2111879878365c90c9e9b07
Manakal <dp.manakal82@gmail.com>
2021-07-20 18:41:20 +0530

    Mutations Added (createProduct)

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -7,7 +7,14 @@ import products from "../models/products";
             return products.find();
         }
     },
-    
+     Mutation:{
+         createProduct:async (parent,args,context,info)=>{
+             const{id,name,category,qty,price,discount,total}=args.product
+             const prod= new products({id,name,category,qty,price,discount,total})
+             await prod.save()
+             return prod
+         }
+     }
 };
 
 module.exports={resolvers};
